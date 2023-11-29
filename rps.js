8f610ff7c03f97ea0cd5f6518cc4af5ca14766f7
Alison D <146018110+ardesroches87@users.noreply.github.com>
2023-11-29 14:30:19 -0400

    Complete the rock paper scissors game

--- rps.js
+++ rps.js
@@ -1,39 +1,105 @@
-//Assign 1 variable as rock
-//Assign 1 variable as paper
-//Assign 1 variable scissors --- made an array instead
-//Assign values to each variable (what beats what) -- if...else if?
-//Prompt user to choose one of the variables (rock, paper, or scissors) - OK
-//Input from computer to randomly choose a variable - OK
-//Compare the inputted variables and decide which is the winner- ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper"))
-//Notify the winner "you won" (notify loser "you lost")
-//Ask to play again
+//Have 3 choices for computer (rock, paper, scissors)
+//Get user to input choice
+//Randomize computer choice
+//Display user choice and computer choice
+//Compare choices and decide which won
+//Repeat 5 times
+//Count number of wins each got
+//Alert whoever has most wins as winner
 
 const choices = ["rock", "paper", "scissors"];
 
+function getPlayerChoice() {
 let playerInput = prompt("Choose rock, paper, or scissors");
-
 let playerSelection = playerInput.toLowerCase();
-console.log(playerSelection);
+console.log(`The player's selection is ${playerSelection}`);
+return playerSelection;
+};
+
 
 function getComputerChoice() {
-    console.log(choices[(Math.floor(Math.random() * choices.length))]);
-}
-getComputerChoice();
+    let result = choices[(Math.floor(Math.random() * choices.length))]
+    console.log(`The computer's selection is ${result}`);
+    return result;
+};
 
 
-let computerSelection = getComputerChoice;
 
-//this fxn isnt working, it's constantly saying it's a tie no matter what, so the if/else ifs have a problem
 function playRound(playerSelection, computerSelection) {
      if (playerSelection === computerSelection) {
-        alert("it's a tie, try again");
+      // console.log(`The player's selection is ${playerSelection} and the computer's selection is ${computerSelection}`)
+      // console.log("it's a tie, try again") 
+      alert("it's a tie, try again");
+      return "tie";
      } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-        alert("player won!");
+      // console.log("player won!")  
+      alert("player won!");
+      return "player won!";
      } else {
-        alert("computer won!");
+      // console.log("computer won!")  
+      alert("computer won!");
+      return "computer won!";
      }
-    }
+    };
 
-     playRound();
+   //  let firstPlayerChoice = getPlayerChoice();
+   //  let firstComputerChoice = getComputerChoice();
 
+   //  let secondPlayerChoice = getPlayerChoice();
+   // let secondComputerChoice = getComputerChoice();
+
+   // let thirdPlayerChoice = getPlayerChoice();
+   //    let thirdComputerChoice = getComputerChoice();
+
+   //    let fourthPlayerChoice = getPlayerChoice();
+   //    let fourthComputerChoice = getComputerChoice();
+
+   //    let fifthPlayerChoice = getPlayerChoice();
+   //    let fifthComputerChoice = getComputerChoice();
+
+ function game() {
+   let winners = [];
+
+
+   let firstPlayerChoice = getPlayerChoice();
+   let firstComputerChoice = getComputerChoice();
+      winners.push(playRound(firstPlayerChoice, firstComputerChoice));
+      // console.log(playRound(firstPlayerChoice, firstComputerChoice));
+
+      let secondPlayerChoice = getPlayerChoice();
+      let secondComputerChoice = getComputerChoice();
+      winners.push(playRound(secondPlayerChoice, secondComputerChoice));
+      // console.log(playRound(secondPlayerChoice, secondComputerChoice));
+
+      let thirdPlayerChoice = getPlayerChoice();
+      let thirdComputerChoice = getComputerChoice();
+      winners.push(playRound(thirdPlayerChoice, thirdComputerChoice));
+      // console.log(playRound(thirdPlayerChoice, thirdComputerChoice));
+
+      let fourthPlayerChoice = getPlayerChoice();
+      let fourthComputerChoice = getComputerChoice();
+      winners.push(playRound(fourthPlayerChoice, fourthComputerChoice));
+      // console.log(playRound(fourthPlayerChoice, fourthComputerChoice));
+
+      let fifthPlayerChoice = getPlayerChoice();
+      let fifthComputerChoice = getComputerChoice();
+      winners.push(playRound(fifthPlayerChoice, fifthComputerChoice));
+      // console.log(playRound(fifthPlayerChoice, fifthComputerChoice));
+
+      console.log(winners);
+
+      let numPlayerWins = winners.filter(score => score === "player won!").length;
+      let numComputerWins = winners.filter(score => score === "computer won!").length;
+      
+
+      if (numPlayerWins > numComputerWins) {
+         alert("you won HELL YEAH!"); 
+      } else if (numComputerWins > numPlayerWins) {
+         alert("you lost! computer won!");
+      } else {
+         alert("it's a tie, try again");
+      }
+      }
+
+     game();
      
