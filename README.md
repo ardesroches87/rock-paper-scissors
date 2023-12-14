e5cb92033035ac2f156106a41fecec848f64af39
Alison D <146018110+ardesroches87@users.noreply.github.com>
2023-12-14 16:33:58 -0400

    Add UI to game

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # rock-paper-scissors
 First attempt at using js to make something. 
 Stuck on the function that compares player input to computer input.
+
+Sources:
+
+Images: Clker-free-vector-images
+Rock image: https://pixabay.com/vectors/rock-paper-scissors-rock-hand-296854/
+Paper image: https://pixabay.com/vectors/rock-paper-scissors-paper-hand-296855/
+Scissors image: https://pixabay.com/vectors/rock-paper-scissors-scissors-hand-296853/
--- rps.js
+++ rps.js
@@ -1,12 +1,13 @@
 
 const choices = ["rock", "paper", "scissors"];
 
-function getPlayerChoice() {
-let playerInput = prompt("Choose rock, paper, or scissors");
-let playerSelection = playerInput.toLowerCase();
-console.log(`The player's selection is ${playerSelection}`);
-return playerSelection;
-};
+// function getPlayerChoice() {
+// let playerInput = prompt("Choose rock, paper, or scissors");
+// let playerSelection = playerInput.toLowerCase();
+// console.log(`The player's selection is ${playerSelection}`);
+// return playerSelection;
+// };
+let results = document.querySelector('#results');
 
 
 function getComputerChoice() {
@@ -14,54 +15,80 @@ function getComputerChoice() {
     console.log(`The computer's selection is ${result}`);
     return result;
 };
-
+let computerSelection = getComputerChoice();
 
 
 function playRound(playerSelection, computerSelection) {
      if (playerSelection === computerSelection) {
       // console.log(`The player's selection is ${playerSelection} and the computer's selection is ${computerSelection}`)
-      // console.log("it's a tie, try again") 
-      alert("it's a tie, try again");
+      console.log("it's a tie, try again");
       return "tie";
      } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
-      // console.log("player won!")  
-      alert("player won!");
+      console.log("player won!");  
       return "player won!";
      } else {
-      // console.log("computer won!")  
-      alert("computer won!");
+      console.log("computer won!");
       return "computer won!";
      }
     };
 
+    let winners = [];
 
-
-   let winners = [];
- function game() {
+    function game(playerSelection, computerSelection) {
+     console.log(playerSelection);
+     console.log(computerSelection);
+      
+      winners.push(playRound(playerSelection, computerSelection));
    
-for (let i = 0; i <= 4; i++) {
-   let myChoice = getPlayerChoice();
-   let compChoice = getComputerChoice();
-      winners.push(playRound(myChoice, compChoice));
-
-      console.log(winners);
-
-}
       let numPlayerWins = winners.filter(score => score === "player won!").length;
       let numComputerWins = winners.filter(score => score === "computer won!").length;
-      
+   
+   console.log(numPlayerWins);
+      console.log(numComputerWins);
+      const para = document.createElement('p');
 
-      if (numPlayerWins > numComputerWins) {
-         alert("you won the round, HELL YEAH!"); 
-         return "you won the round,  HELL YEAH!";
-      } else if (numComputerWins > numPlayerWins) {
-         alert("you lost the round! computer won!");
-         return "you lost the round! computer won!";
-      } else {
-         alert("it's a tied round, try again");
-         return "it's a tied round, try again";
+      if (numPlayerWins < 5 && numComputerWins < 5) {
+        para.textContent = `You chose ${playerSelection} and computer chose ${computerSelection}. You have ${numPlayerWins} points and the computer has ${numComputerWins} points.`;
+      } else if (numPlayerWins === 5 && numComputerWins < 5) {
+         para.textContent = `You win!: ${numPlayerWins} - ${numComputerWins}`;
+      } else if (numPlayerWins < 5 && numComputerWins === 5) {
+         para.textContent = `You lose! Computer wins!: ${numComputerWins} - ${numPlayerWins}`;
       }
-      
+      results.insertAdjacentElement('afterbegin', para);
+
    }
 
-     game();
+
+
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+   game("rock", getComputerChoice())
+});
+
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+   game("paper", getComputerChoice())
+});
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+   game("scissors", getComputerChoice())
+});
+
+ 
+      
+
+      // if (numPlayerWins > numComputerWins) {
+      //    alert("you won the round, HELL YEAH!"); 
+      //    return "you won the round,  HELL YEAH!";
+      // } else if (numComputerWins > numPlayerWins) {
+      //    alert("you lost the round! computer won!");
+      //    return "you lost the round! computer won!";
+      // } else {
+      //    alert("it's a tied round, try again");
+      //    return "it's a tied round, try again";
+      // }
+      
+   
+
+     
